c131fe14f2d2cf5622f248871a635f786e77b5e4
Piotr Janaszek <piotr.janaszek@gmail.com>
2026-03-18 11:05:22 +0100

    Exercism 3

--- etl/etl.py
+++ etl/etl.py
@@ -1,6 +1,2 @@
 def transform(legacy_data):
-    data = {}
-    for index, value_list in legacy_data.items():
-        for value in value_list:
-            data.setdefault(str(value).lower(), index)
-    return data
+    return {letter.lower(): key for key, value in legacy_data.items() for letter in value}
